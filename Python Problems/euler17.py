051b5125837a69848696754c62705be178b6a6e3
Jay Shapiro <jas2dp@virginia.edu>
2020-03-17 22:08:31 -0400

    Add files via upload

--- Python Problems/euler17.py
+++ Python Problems/euler17.py
@@ -1,2 +1,13 @@
-# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+# If all the numbers from 1 to 1000 (one thousand) inclusive were 
+# written out in words, how many letters would be used?
+
+#Length of the first 19 numbers
+sub_twenty = [len("one"), len("two"), len("three"), len("four"), len("five"),
+len("six"), len("seven"), len("eight"), len("nine"), len("ten"), len("eleven"),
+len("twelve"), len("thirteen"), len("fourteen"), len("fifteen"), len("sixteen"),
+len("seventeen"),len("eighteen"), len("nineteen")]
+
+#Length of the tens' place
+tens = [len("twenty"), len("thirty"), len("forty"), len("fifty"), len("sixty"),
+len("seventy"), len("eighty"), len("ninety")]
 
