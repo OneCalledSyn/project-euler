39b1b4c8d3c4912f273480f7e68130f78e2731de
Jay Shapiro <jas2dp@virginia.edu>
2020-06-02 19:45:45 -0400

    Add files via upload

--- Python Problems/euler21.py
+++ Python Problems/euler21.py
@@ -1,7 +1,19 @@
-# Let d(n) be defined as the sum of proper divisors of n.
-# If d(a) = b and d(b) = a, where a ??? b,
-# then a and b are an amicable pair and a and b are called 'amicable numbers'.
 # Evaluate the sum of all the amicable numbers under 10000.
+# Done jointly with Legman
 
-import math
+def get_divisors(n):
+  divisor_pairs = [[x, int(n / x)] for x in range(2, int(n / 4) + 1) if n % x == 0]
+  return set([divisor for divisor_pair in divisor_pairs for divisor in divisor_pair] + [1])
 
+def get_amicable_numbers(n):
+  amicable_numbers = []
+  for i in range(1, n + 1):
+    sum_of_divisors = sum(get_divisors(i))
+    if sum_of_divisors == i:
+      continue
+    potential_amicable_sum = sum(get_divisors(sum_of_divisors))
+    if potential_amicable_sum == i:
+      amicable_numbers.append(i)
+  return amicable_numbers
+
+print(sum(get_amicable_numbers(10000)))
