7c1063db5e4bf9fc37b9cb4c7e86e994aabab574
Jay Shapiro <jas2dp@virginia.edu>
2023-07-23 15:30:36 -0400

    Add files via upload
    added iterative solution

--- Python Problems/719.py
+++ Python Problems/719.py
@@ -1,13 +1,42 @@
-def is_snumber(x, y):
-    if x == y:
-        return True
+# Recursive solution
 
-    t = 10
-    while t < y:
-        q, r = divmod(y, t)
-        if r < x and is_snumber(x - r, q):
+# def is_snumber(x, y):
+#     if x == y:
+#         return True
+
+#     t = 10
+#     while t < y:
+#         q, r = divmod(y, t)
+#         if r < x and is_snumber(x - r, q):
+#             return True
+#         t *= 10
+    
+#     return False
+
+# total = 0
+# for i in range(4, 10**6 + 1):
+#     if is_snumber(i, i*i):
+#         total += i*i
+
+# print(total)
+
+# Iteration solution
+
+def is_snumber(x, y):
+    stack = [(x, y)]
+
+    while stack:
+        x, y = stack.pop()
+        
+        if x == y:
             return True
-        t *= 10
+        
+        t = 10
+        while t < y:
+            q, r = divmod(y, t)
+            if r < x:
+                stack.append((x - r, q))
+            t *= 10
     
     return False
 
